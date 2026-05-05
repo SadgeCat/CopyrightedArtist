99d6219ce724ff6873af39405b9f1aec46092cbc
Yuhang <yuhangpan2@gmail.com>
2026-05-05 11:44:29 -0400

    add

--- app/lobby.py
+++ app/lobby.py
@@ -1,3 +1,12 @@
-class lobby
+class lobby:
+    def __init__(self):
+        self.lobbies = {}
 
-def __init__(self):
+    def create_lobby(self, user_id, lobby_id):
+        self.lobbies[lobby_id] = {
+            players = [user_id],
+            host = [user_id]
+        }
+
+    def join_lobby(self, user_id, lobby_id):
+        self.lobbies[lobby_id][]
