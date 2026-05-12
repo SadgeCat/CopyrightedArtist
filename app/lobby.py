d14f7dccd5f90750a3c05580f4ca0386d2551158
Zixi Qiao <qiaozixi@gmail.com>
2026-05-12 11:38:25 -0400

    initial lobby funcs

--- app/lobby.py
+++ app/lobby.py
@@ -1,7 +1,7 @@
 from flask_socketio import SocketIO
 
 class lobby:
-    def __init__(self):
+    def __init__(self, socketIO):
         self.lobbies = {}
 
     def create_lobby(self, user_id, lobby_id):
@@ -12,3 +12,22 @@ class lobby:
 
     def join_lobby(self, user_id, lobby_id):
         self.lobbies[lobby_id][]
+
+    @socketio.on('lobby')
+    def lobby(id):
+    print("We are in new lobby maybe "+str(id))
+    emit('chat', lobby['id'], broadcast=True, to=lobby['id'])
+
+    @socketio.on('join')
+    def on_join(data):
+        username = data['username']
+        room = data['room']
+        join_room(room)
+        send(username + ' has entered the room.', to=room)
+
+    @socketio.on('leave')
+    def on_leave(data):
+        username = data['username']
+        room = data['room']
+        leave_room(room)
+        send(username + ' has left the room.', to=room)
